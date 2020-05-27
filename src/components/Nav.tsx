396b3c41820e6fa03e6f34056c5d413778369acb
qwxying <qwxying@gmail.com>
2020-05-27 14:47:49 +0800

    用svgo-loader 引入icons

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,41 +2,67 @@ import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
 
+require('icons/home.svg');
+require('icons/details.svg');
+require('icons/statistics.svg');
+require('icons/users.svg');
+
+
 const NavWrapper = styled.nav`
-line-height: 24px;
+  line-height: 24px;
   box-shadow: 0 0 3px rgba(0,0,0,0.25);
   > ul {
     display: flex;
     align-items: center;
     //height: 72px;
     > li {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
       width: 25%;
       text-align: center;
-      padding: 16px;
+      padding: 8px;
+      .icon{
+        width: 24px;     
+        height: 24px;   
+      }
     }
   }
 `;
 
-const Nav = () =>{
-  return(
+const Nav = () => {
+  return (
     <NavWrapper>
       <ul>
         <li>
+          <svg className='icon'>
+            <use xlinkHref="#home"/>
+          </svg>
           <Link to="/home">首页</Link>
         </li>
         <li>
+          <svg className='icon'>
+            <use xlinkHref="#details"/>
+          </svg>
           <Link to="/details">明细</Link>
         </li>
         <li>
+          <svg className='icon'>
+            <use xlinkHref="#statistics"/>
+          </svg>
           <Link to="/statistics">统计</Link>
         </li>
         <li>
+          <svg className='icon'>
+            <use xlinkHref="#users"/>
+          </svg>
           <Link to="/users">我的</Link>
         </li>
       </ul>
     </NavWrapper>
-  )
-}
+  );
+};
 
 
 export default Nav;
